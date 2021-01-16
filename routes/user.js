090c41da96a33e8df92328e3322f2b9a1d632912
Takuto Okamoto <takuto.okamoto@bstarsolutions.com>
2021-01-15 19:59:47 -0500

    Update User Route

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 const User = require('../models/User');
 
 // <<< Delete Later >>>
@@ -46,5 +47,41 @@ router.get('/:userId', async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
+
+// @Route  PUT api/user/:userId
+// @desc   Get user
+// @access Private
+router.put('/:userId',
+  [
+    check('name', 'User Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      let user = await User.findById(req.params.userId);
+
+      if (!user) {
+        return res.status(404).json({ msg: 'User does not exists '})
+      }
+
+      user = await User.findOneAndUpdate(
+        req.params.userId,
+        { new: true }
+      );
+
+      return res.json(user);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
