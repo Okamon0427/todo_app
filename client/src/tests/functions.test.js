f20b71a856e87b35d47cc9359719abd388657fb7
takuto-okamoto <okamonisfire@yahoo.co.jp>
2021-02-19 23:29:52 -0500

    Add Test for Search Bar

--- client/src/tests/functions.test.js
+++ client/src/tests/functions.test.js
@@ -1,4 +1,4 @@
-import { setTokenToHeader } from '../utils/functions';
+import { setTokenToHeader, searchTodos } from '../utils/functions';
 import axios from 'axios';
 
 it('set axios headers if token is passed', () => {
@@ -13,4 +13,16 @@ it('delete axios headers if no token is passed', () => {
   expect(axios.defaults.headers.common['x-auth-token']).toBe('abc');
   setTokenToHeader();
   expect(axios.defaults.headers.common['x-auth-token']).toBeUndefined();
+});
+
+it('return filtered array regardless of case', () => {
+  const todos = [
+    { title: 'Good morning' },
+    { title: 'Hello' },
+    { title: 'Good night' }
+  ];
+  const event = { target: { value: 'hello' } };
+  const expected = [{"title": "Hello"}];
+
+  expect(searchTodos(todos, event)).toEqual(expect.arrayContaining(expected));
 });
